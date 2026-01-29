85f5329b28fec90dae3eb49f7b0748bd8156cd4c
Damon Fouch <damonfouch@MacBookPro.mynet>
2026-01-29 10:41:56 +0000

    fix: restore 'use client' for program detail pages

--- src/app/track-a/programs/[id]/page.tsx
+++ src/app/track-a/programs/[id]/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useState } from 'react';
 import { programsData } from '@/lib/programData';
 import Link from 'next/link';
--- src/app/track-b/programs/[id]/page.tsx
+++ src/app/track-b/programs/[id]/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useState } from 'react';
 import { gymnasticsData } from '@/lib/gymnasticsData';
 import Link from 'next/link';
